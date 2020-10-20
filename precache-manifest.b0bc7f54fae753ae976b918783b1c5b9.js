d5819a196b35c3c98810f425556707a61fda3bce
tomfrancix <58977284+tomfrancix@users.noreply.github.com>
2020-10-20 14:08:40 +0100

    Updates

---
+++ precache-manifest.b0bc7f54fae753ae976b918783b1c5b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31dd35b7d8d29e407989dcab7832fc5",
+    "url": "/Robot-Filter-React-App/index.html"
+  },
+  {
+    "revision": "0556eed029c159efc285",
+    "url": "/Robot-Filter-React-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4401210d8d03e3304417",
+    "url": "/Robot-Filter-React-App/static/css/main.26882071.chunk.css"
+  },
+  {
+    "revision": "0556eed029c159efc285",
+    "url": "/Robot-Filter-React-App/static/js/2.8fa0d876.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Robot-Filter-React-App/static/js/2.8fa0d876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4401210d8d03e3304417",
+    "url": "/Robot-Filter-React-App/static/js/main.6a7d3e23.chunk.js"
+  },
+  {
+    "revision": "2d88d978d5656c78ede8",
+    "url": "/Robot-Filter-React-App/static/js/runtime-main.100a097e.js"
+  }
+]);
